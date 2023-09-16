bf0c047578bbd29ef30f7e5915316b6fd66b7310
khalilnazari <khalil.deve@gmail.com>
2023-09-16 14:15:25 +0800

    UserAuth component is created

---
+++ src/app/(auth)/sign-up/page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+type Props = {};
+
+const SingUpPage = (props: Props) => {
+  return <div>SingUpPage</div>;
+};
+
+export default SingUpPage;
--- src/components/UserAuthForm.tsx
+++ src/components/UserAuthForm.tsx
@@ -1,9 +1,43 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
+import { cn } from "@/lib/utils";
+import { signIn } from "next-auth/react";
+import { Icons } from "./Icons";
+import { Button } from "./ui/Button";
 
-type Props = {};
+interface UserAuthFormProps extends React.HTMLAttributes<HTMLDivElement> {}
 
-const UserAuthForm = (props: Props) => {
-  return <div>UserAuthForm</div>;
+const UserAuthForm = ({ className, ...props }: UserAuthFormProps) => {
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
+  const loginWithGoogle = async () => {
+    setIsLoading(true);
+
+    try {
+      Error();
+      return;
+      await signIn("google");
+    } catch (error) {
+      alert(JSON.stringify(error));
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  return (
+    <div className={cn("flex justify-center", className)} {...props}>
+      <Button
+        isLoading={isLoading}
+        type="button"
+        className="w-full"
+        onClick={loginWithGoogle}
+        disabled={isLoading}
+      >
+        {isLoading ? null : <Icons.google className="h-4 w-4 mr-2" />}
+        Google
+      </Button>
+    </div>
+  );
 };
 
 export default UserAuthForm;
